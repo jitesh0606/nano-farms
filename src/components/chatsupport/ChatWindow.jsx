c53677a2ac7875c5951e876beffcf42c776d8178
Jitesh Singh <github-wali-email@gmail.com>
2026-08-04 23:51:52 +0530

    Added outside click to close chat window

--- src/components/chatsupport/ChatWindow.jsx
+++ src/components/chatsupport/ChatWindow.jsx
@@ -1,20 +1,33 @@
 import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
 import CustomerForm from "./CustomerForm";
+import { useEffect, useRef } from "react";
 const ChatWindow = ({ onClose, messages, onSend }) => {
-  return (
-    <div className="chat-window">
+  const chatRef = useRef(null);
 
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (chatRef.current && !chatRef.current.contains(e.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, [onClose]);
+
+  return (
+    <div className="chat-window" ref={chatRef}>
       <div className="chat-header">
         <h3>🌿 Nano Farms Support</h3>
-
         <button onClick={onClose}>✖</button>
       </div>
 
       <MessageList messages={messages} />
-
       <MessageInput onSend={onSend} />
-
     </div>
   );
 };
